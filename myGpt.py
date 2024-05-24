8b8f30c8c46c8c84b07dfc646071d657208d34ae
nurciuoli <naurciuoli@gmail.com>
2024-05-24 15:59:35 -0400

    opt

--- myGpt.py
+++ myGpt.py
@@ -27,49 +27,40 @@ def append_content_w_images(prompt,images):
  
 # Assistant Functions
 # Initialize Assistant
-def initialize_assistant(agent):
+def initialize_assistant(name,system_prompt,tools,model):
     print('cp: initializing agent')
     try:
-        if agent.tools is not None:
-            agent.assistant = client.beta.assistants.create(
-                name=agent.name,
-                instructions=agent.system_prompt,
-                tools=agent.tools,
-                model=agent.model,
+        if tools is not None:
+            assistant = client.beta.assistants.create(
+                name=name,
+                instructions=system_prompt,
+                tools=tools,
+                model=model,
                 )
         else:
-            agent.assistant = client.beta.assistants.create(
-                name=agent.name,
-                instructions=agent.system_prompt,
-                model=agent.model,
+            assistant = client.beta.assistants.create(
+                name=name,
+                instructions=system_prompt,
+                model=model,
             )
         print("cp: assistant initialized")
+        return assistant
     except Exception as e:
         # handle the exception
         print(f"Failed to initailize agent: Caught {e.__class__.__name__}: {e}")
 
 # Initialize Thread
-def initialize_thread(agent):
+def initialize_thread():
     try:
         print('cp: creating thread')
-        agent.thread = client.beta.threads.create()
+        thread = client.beta.threads.create()
         print('cp: thread created')
+        return thread
     except Exception as e:
         # handle the exception
         print(f"Failed to initailize thread: Caught {e.__class__.__name__}: {e}")
 
-# Go through messages and print content
-def add_msg_content_to_list(messages):
-    out_msgs = []
-    for full_msg in json.loads(messages.json())['data']:
-        role = full_msg['role']
-        for message in full_msg['content']:
-            if message['type']=='text':
-                print(role+': '+message['text']['value'])
-                out_msgs.append(message['text']['value'])
-    
-    return out_msgs
-
+# run steps
 def get_run_steps(run,thread_id):
     # Retrieve and process the run steps
     return client.beta.threads.runs.steps.list(thread_id=thread_id, run_id=run.id, order="asc")
@@ -81,16 +72,19 @@ def go_through_tool_actions(tool_calls,run,thread_id):
         function_name = json.loads(tool_call.json())['function']['name']
         print(f'cp: {function_name}')
         tool_output_list.append({"tool_call_id": tool_call.id,"output": "the work is done and in the project folder"})
-
-
     # Submit the collected tool outputs and return the run object
     run = client.beta.threads.runs.submit_tool_outputs(thread_id=thread_id, run_id=run.id, tool_outputs=tool_output_list)
-
     print('cp: done going through tool actions')
-
     return run
+
+def get_messages_from_thread(thread_id,order = 'asc'):
+    return client.beta.threads.messages.list(
+                thread_id=thread_id,
+                order = order,
+            )
             
 def print_messages_out(messages):
+    print('=========================================')    
     print("               messages ")
     print('-----------------------------------------')
     for message in messages:
@@ -98,35 +92,21 @@ def print_messages_out(messages):
         msg_value = message.content[0].text.value
         msg_role = message.role
         print(f'{msg_role}: {msg_value}')
+    print('=========================================')
+        
 
-# Agent class
-class Agent:
-    def __init__(self,system_prompt="You are a helpful chat based assistant",
-                 name = 'AgentGptnewnew',
-                 model = 'gpt-3.5-turbo-1106',
-                 tools = None):
-        self.system_prompt = system_prompt
-        self.name = name
-        self.model = model
-        self.tools = tools
-        initialize_assistant(self)
-        self.thread = None
-        self.run=None
-        self.messages=[]
-    # chat method
-
-    def wait_on_run(self,thread_id,additional_instructions=None):
+def wait_on_run(assistant_id,thread_id,additional_instructions=None):
         print(f'cp: waiting on run')
         if additional_instructions is not None:
             run=client.beta.threads.runs.create(
                     thread_id=thread_id,
-                    assistant_id=self.assistant.id,
+                    assistant_id=assistant_id,
                     additional_instructions=additional_instructions,
                 )
         else:
             run=client.beta.threads.runs.create(
                     thread_id=thread_id,
-                    assistant_id=self.assistant.id,
+                    assistant_id=assistant_id,
                 )
         while run.status in ["queued", "in_progress"]:
             # Retrieve updated run status
@@ -152,6 +132,22 @@ class Agent:
 
         print('cp: finished waiting')
         return run
+
+# Agent class
+class Agent:
+    def __init__(self,system_prompt="You are a helpful chat based assistant",
+                 name = 'AgentGpt',
+                 model = 'gpt-3.5-turbo-1106',
+                 tools = None):
+        self.system_prompt = system_prompt
+        self.name = name
+        self.model = model
+        self.tools = tools
+        self.assistant = initialize_assistant(name,system_prompt,tools,model)
+        self.thread = None
+        self.run=None
+        self.messages=[]
+    # chat method
     
     def add_message(self,content):
         message = client.beta.threads.messages.create(
@@ -166,25 +162,13 @@ class Agent:
     def chat(self,msg,additional_prompt = None,images=None,files=None):
         print(f'cp: starting chat')
         if self.thread is None:
-            initialize_thread(self)
+            self.thread=initialize_thread()
 
         if images is not None:
            content =append_content_w_images(msg,images)
         else:
            content = msg
-        
         self.add_message(content)
-
-        self.run = self.wait_on_run(self.thread.id,additional_prompt)
-
-        self.messages = client.beta.threads.messages.list(
-                thread_id=self.thread.id,
-                order = 'asc',
-            )
-        
-        print('=========================================')
-        
+        self.run = wait_on_run(self.assistant.id,self.thread.id,additional_prompt)
+        self.messages = get_messages_from_thread(self.thread.id)
         print_messages_out(self.messages)
-
-        print('=========================================')
-
