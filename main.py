ec911847244dcdb9ef969f0e81e05af551617636
nurciuoli <naurciuoli@gmail.com>
2024-06-15 16:03:37 -0400

    organize

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from agents.myLlama import Agent as LlamaAgent
 from agents.myGemini import Agent as GeminiAgent
 from agents.myClaude import Agent as ClaudeAgent
 from agents.myGpt import Agent as GptAgent
-# App title
 from models import model_ids
 
-
+# Define a dictionary mapping model names to their respective agent classes
 agent_classes = {
     'llama': LlamaAgent,
     'gemini': GeminiAgent,
@@ -15,24 +14,27 @@ agent_classes = {
     'gpt': GptAgent,
 }
 
+# Set the page title and layout
 st.set_page_config(page_title="💬 My Chatbot")
 
-# Replicate Credentials
+# Sidebar configuration
 with st.sidebar:
     st.title('💬 My Chatbot')
     st.write('Fun little chatbot project')
     st.subheader('Models and parameters')
-    selected_model = st.sidebar.selectbox('Choose a model', list(model_ids.keys()), key='selected_model',on_change=lambda: st.session_state.pop('agent', None))
-    maxt=model_ids[selected_model]['max']
-    system_prompt = st.sidebar.text_input('System Prompt', value = "You are a helpful assistant")
+    
+    # Model selection
+    selected_model = st.sidebar.selectbox('Choose a model', list(model_ids.keys()), key='selected_model', on_change=lambda: st.session_state.pop('agent', None))
+    maxt = model_ids[selected_model]['max']
+    
+    # Model parameters
+    system_prompt = st.sidebar.text_input('System Prompt', value="You are a helpful assistant")
     temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=1.0, value=0.1, step=0.01)
     max_length = st.sidebar.slider('max_length', min_value=32, max_value=maxt, value=1000, step=8)
-
 
 # Function for initializing or re-initializing the agent
 def initialize_agent():
     AgentClass = agent_classes[model_ids[selected_model]['vendor']]
-    # Exclude the first placeholder message by slicing the list from the second element
     message_history = st.session_state.messages[1:] if len(st.session_state.messages) > 1 else []
     st.session_state.agent = AgentClass(model=selected_model,
                                         max_tokens=max_length,
@@ -48,34 +50,32 @@ if selected_model != st.session_state.previous_model:
     initialize_agent()
     st.session_state.previous_model = selected_model
 
-
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [{"role": "assistant", "content": system_prompt}]
 
-# Display or clear chat messages
+# Display chat messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
+# Clear chat history
 def clear_chat_history():
     try:
         st.session_state.messages = [{"role": "assistant", "content": system_prompt}]
-    
         if "agent" in st.session_state.keys():
             st.session_state.pop('agent')
     except:
         print('failed to clear chat')
+
 st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
-# Function for generating response
 # Function for generating response
 def generate_response(prompt_input):
     try:
         if "agent" not in st.session_state.keys():
             AgentClass = agent_classes[model_ids[selected_model]['vendor']]
             try:
-                # Exclude the first placeholder message by slicing the list from the second element
                 message_history = st.session_state.messages[1:] if len(st.session_state.messages) > 1 else []
                 st.session_state.agent = AgentClass(model=selected_model,
                                                     max_tokens=max_length,
